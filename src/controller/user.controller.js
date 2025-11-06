dfb3cdf4a1508d6439414f1169e809a43de78eba
Solanki Manan <solankimanan19@gmail.com>
2025-11-06 14:11:55 +0530

    Fixed All Register Controller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -16,27 +16,38 @@ const registerUser = asyncHandler(async (req, res) => {
     // return res 
 
 
-    const { fullName, email, username, password } = req.body
+    const { fullName, email, userName, password } = req.body
     console.log("Email ", email);
 
-    if ([fullName, email, username, password].some((field) => field?.trim() === "")) {
+    if ([fullName, email, userName, password].some((field) => field?.trim() === "")) {
         throw new ApiError(400, "All fields are required");
     }
 
-    const existedUser = User.findOne({
-        $or: [{ username }, { email }]
+    const existedUser = await User.findOne({
+        $or: [{ userName }, { email }]
     })
 
     if(existedUser){
         throw new ApiError(409 , "User with email or username is exists");
     }
 
+    console.log(req.files);
+
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    
 
     if(!avatarLocalPath){
         throw new ApiError(400 , "Avatar file is required")
     }
+
+    let coverImageLocalPath;
+
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0 ){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+     
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
@@ -51,7 +62,7 @@ const registerUser = asyncHandler(async (req, res) => {
         coverImage : coverImage?.url || "",
         email ,
         password,
-        username : username.toLowerCase()
+        userName : userName?.toLowerCase()
     })
 
     const createdUser = await User.findById(user._id).select(
@@ -65,13 +76,6 @@ const registerUser = asyncHandler(async (req, res) => {
     return res.status(201).json(
         new ApiResponse(200 , createdUser , "User registered succssfully")
     )
-
-
-
-
-
-
-
 
 })
 
